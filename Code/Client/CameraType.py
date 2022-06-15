d8d41d647545c093d05542f180304b60c74f8f3c
Daniel <daniel.grigsby.03@gmail.com>
2022-06-15 15:10:25 -0400

    yes

---
+++ Code/Client/CameraType.py
@@ -0,0 +1,9 @@
+class CameraType:
+    def __init__(self):
+        self.word = ""
+    
+    def getType(self):
+        return self.word
+    
+    def setType(self, word1):
+        self.word = word1
