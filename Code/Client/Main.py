f92d6fd78a52070a40f1b0b3b854d46499f48d29
1medlinj <1medlinj@gmail.com>
2022-06-22 14:36:23 -0400

    pain

--- Code/Client/Main.py
+++ Code/Client/Main.py
@@ -743,8 +743,8 @@ class mywindow(QMainWindow,Ui_Client):
         if face_x!=0 and face_y!=0:
             offset_x=float(face_x/400-0.5)*2
             offset_y=float(face_y/300-0.5)*2
-            delta_degree_x = 4* offset_x
-            delta_degree_y = -4 * offset_y
+            delta_degree_x = 2* offset_x
+            delta_degree_y = -2 * offset_y
 
             self.servo1=self.servo1+delta_degree_x
             self.servo2=self.servo2+delta_degree_y
